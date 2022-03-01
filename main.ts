b10d07b0c8a42346d52b876a2a78d7382765be70
beck30207 <99796719+beck30207@users.noreply.github.com>
2022-03-01 10:46:25 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    targrt += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(targrt)
+})
+input.onButtonPressed(Button.B, function () {
+    targrt += 1
+})
+let targrt = 0
+basic.showIcon(IconNames.Heart)
 radio.setGroup(99)
+targrt = 50
 basic.forever(function () {
 	
 })
